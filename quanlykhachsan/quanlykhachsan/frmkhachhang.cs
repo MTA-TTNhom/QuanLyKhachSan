90b4ea5a8fada0e2e75027a3d33829c784526af6
asdcxsd <nguyenmanhthien98@gmail.com>
2019-08-22 08:36:22 +0700

    update edit form khach hang ( giao dien)

--- quanlykhachsan/quanlykhachsan/frmkhachhang.cs
+++ quanlykhachsan/quanlykhachsan/frmkhachhang.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL;
 
 namespace quanlykhachsan
 {
@@ -15,6 +16,54 @@ namespace quanlykhachsan
         public frmkhachhang()
         {
             InitializeComponent();
+        }
+        int temp;
+        private void hienthi()
+        {
+
+        }
+        private void trangthai(bool t)
+        {
+
+        }
+
+        private void frm_load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private bool travegioitinh()
+        {
+     
+        }
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnluu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
