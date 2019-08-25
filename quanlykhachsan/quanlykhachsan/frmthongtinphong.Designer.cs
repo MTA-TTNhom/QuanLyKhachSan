a8314260b7518c68d6eea11ddbcab135d6f1f13c
Hoangthuy125 <thuy94.mta@gmail.com>
2019-08-25 09:20:30 +0700

    update giao diện

--- quanlykhachsan/quanlykhachsan/frmthongtinphong.Designer.cs
+++ quanlykhachsan/quanlykhachsan/frmthongtinphong.Designer.cs
@@ -49,6 +49,17 @@
             this.donGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.GridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(294, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(277, 31);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "THÔNG TIN PHÒNG";
         }
 
         #endregion
