6d723eefde7de3bb509c7b8c5370b8ebe7b8264d
Hoangthuy125 <thuy94.mta@gmail.com>
2019-08-27 09:36:02 +0700

    update giao diện

--- quanlykhachsan/quanlykhachsan/frmthongtinpphongkh.Designer.cs
+++ quanlykhachsan/quanlykhachsan/frmthongtinpphongkh.Designer.cs
@@ -28,7 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-           
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.maPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loaiPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tinhTrang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.donGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.thoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.maPhong,
+            this.loaiPhong,
+            this.tinhTrang,
+            this.donGia});
+            this.dataGridView1.Location = new System.Drawing.Point(1, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(772, 353);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // maPhong
+            // 
+            this.maPhong.DataPropertyName = "maPhong";
+            this.maPhong.HeaderText = "Phòng";
+            this.maPhong.Name = "maPhong";
+            this.maPhong.ReadOnly = true;
+            // 
+            // loaiPhong
+            // 
+            this.loaiPhong.DataPropertyName = "loaiPhong";
+            this.loaiPhong.HeaderText = "Loại Phòng";
+            this.loaiPhong.Name = "loaiPhong";
+            this.loaiPhong.ReadOnly = true;
         }
 
         #endregion
