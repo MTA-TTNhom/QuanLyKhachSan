7a0bf830ff665ae205bbf92b5c4b2cbbab000615
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-09-02 10:01:27 +0700

    update giao dien

--- quanlykhachsan/BLL/thuephongBLL.cs
+++ quanlykhachsan/BLL/thuephongBLL.cs
@@ -27,3 +27,26 @@ namespace BLL
         }
     }
 }
+
+//Hoang chinh sua
+DataService ds = new DataService();
+public DataTable LayDSTinhTrangPhong()
+{
+    SqlCommand cmd = new SqlCommand("select * from LOAI_TINH_TRANG");
+    ds.Load(cmd);
+    return ds;
+}
+public DataRow NewRow()
+{
+    return ds.NewRow();
+}
+
+public void Add(DataRow row)
+{
+    ds.Rows.Add(row);
+}
+
+public bool Save()
+{
+    return (ds.ExecuteNoneQuery() > 0);
+}
