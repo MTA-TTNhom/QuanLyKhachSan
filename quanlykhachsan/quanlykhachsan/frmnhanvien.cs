b4e3c9d57431b7d9de55ff18aeca265ed2dbaa99
Hoangthuy125 <thuy94.mta@gmail.com>
2019-09-05 22:16:40 +0700

    update nhanvien

--- quanlykhachsan/quanlykhachsan/frmnhanvien.cs
+++ quanlykhachsan/quanlykhachsan/frmnhanvien.cs
@@ -24,7 +24,6 @@ namespace quanlykhachsan
         }
         private void trangthai(bool t)
         {
-            //  txtmanv.Enabled = t;
             txttennv.Enabled = t;
             txtscm.Enabled = t;
             txtsdt.Enabled = t;
@@ -32,7 +31,27 @@ namespace quanlykhachsan
             dtngaysinh.Enabled = t;
             dtngayvaolam.Enabled = t;
             cbgioitinh.Enabled = t;
-
+        }
+        private void frm_load(object sender, EventArgs e)
+        {
+            nhanvienBLL pbll = new nhanvienBLL();
+            dataGridView1.DataSource = pbll.hienthinhanvien();
+            trangthai(false);
+            btnluu.Enabled = false;
+            btnhuy.Enabled = false;
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row = dataGridView1.Rows[e.RowIndex];
+            txtmanv.Text = row.Cells["maNhanVien"].Value.ToString();
+            txttennv.Text = row.Cells["hoTen"].Value.ToString();
+            txtscm.Text = row.Cells["soChungMinh"].Value.ToString();
+            txtsdt.Text = row.Cells["soDienThoai"].Value.ToString();
+            txtdiachi.Text = row.Cells["diaChi"].Value.ToString();
+            cbgioitinh.Text = row.Cells["gioiTinh"].Value.ToString();
+            dtngaysinh.Value = Convert.ToDateTime(row.Cells["ngaySinh"].Value.ToString());
+            dtngayvaolam.Value = Convert.ToDateTime(row.Cells["ngayVaoLam"].Value.ToString());
         }
     }
 }
