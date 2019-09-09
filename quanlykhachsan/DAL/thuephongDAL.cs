a7d933a4aa47a7139ba1a387275517b06d6debae
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-09 21:28:56 +0700

    Update thuephongDAL.cs

--- quanlykhachsan/DAL/thuephongDAL.cs
+++ quanlykhachsan/DAL/thuephongDAL.cs
@@ -29,5 +29,20 @@ namespace DAL
                 return false;
             }
         }
+        public DataTable hienthi_thuetheophong()
+        {
+            SqlConnection cnn = ketnoi.Get();
+            // Khai báo và khởi tạo đối tượng Command, truyền vào tên thủ tục tương ứng
+            SqlCommand cmd = new SqlCommand("hienthi_thuetheophong", cnn);
+            // Khai báo kiểu thực thi là Thực thi thủ tục
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cnn.Close();
+            return dt;
+        }
     }
 }
