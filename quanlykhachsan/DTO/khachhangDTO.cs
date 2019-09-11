83a7d421721628e601846f3c89b9f05570ef2870
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-12 00:47:25 +0700

    Huynh

--- quanlykhachsan/DTO/khachhangDTO.cs
+++ quanlykhachsan/DTO/khachhangDTO.cs
@@ -8,14 +8,7 @@ namespace DTO
 {
     public class khachhangDTO
     {
-        private int _maKhachHang;
-        private string _tenKhachHang;
-        private bool _gioiTinh;
-        private DateTime _ngaySinh;
-        private string _chungMinhNhanDan;
-        private string _diaChi;
-        private string _soDienThoai;
-        private string _quocTich;
+        
 
         public int maKhachHang
         {
