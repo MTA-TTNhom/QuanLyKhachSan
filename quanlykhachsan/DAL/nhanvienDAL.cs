ec62e1aab8a90bac27190df503e653ed70557974
asdcxsd <nguyenmanhthien98@gmail.com>
2019-09-12 15:18:56 +0700

    Update nhanvienDAL.cs

--- quanlykhachsan/DAL/nhanvienDAL.cs
+++ quanlykhachsan/DAL/nhanvienDAL.cs
@@ -26,6 +26,30 @@ namespace DAL
             cnn.Close();
             return dt;
         }
+		
+		{
+            SqlConnection cnn = ketnoi.Get();
+            SqlCommand cmd = new SqlCommand("them_nhanvien", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("hoTen", hoTen);
+            cmd.Parameters.AddWithValue("gioiTinh", gioiTinh);
+            cmd.Parameters.AddWithValue("ngaySinh", ngaySinh);
+            cmd.Parameters.AddWithValue("soChungMinh", soChungMinh);
+            cmd.Parameters.AddWithValue("diaChi", diaChi);
+            cmd.Parameters.AddWithValue("soDienThoai", soDienThoai);
+            cmd.Parameters.AddWithValue("ngayVaoLam", ngayVaoLam);
+
+            int i = cmd.ExecuteNonQuery();
+            cnn.Close();
+            if (i != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         public bool them_nhanvien(string hoTen, bool gioiTinh, DateTime ngaySinh, string soChungMinh, string diaChi, string soDienThoai, DateTime ngayVaoLam)
         {
             SqlConnection cnn = ketnoi.Get();
