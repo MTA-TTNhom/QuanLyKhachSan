1a18e102252710bf1a56bfee85b41d95f966911c
Hoangthuy125 <thuy94.mta@gmail.com>
2019-09-15 23:13:58 +0700

    update

--- quanlykhachsan/quanlykhachsan/frmmain.Designer.cs
+++ quanlykhachsan/quanlykhachsan/frmmain.Designer.cs
@@ -28,78 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-           
+
+            this.Tabphong = new System.Windows.Forms.TabControl();
+            this.tpDanhSanh = new System.Windows.Forms.TabPage();
+            this.dtgvDanhSachphong = new System.Windows.Forms.DataGridView();
+            this.maPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loaiPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tinhTrang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.donGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btndangxuat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tpThuePhong = new System.Windows.Forms.TabPage();
+            this.bHuyBo = new System.Windows.Forms.Button();
+            this.btThuePhongOK = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.dgkhachhang = new System.Windows.Forms.DataGridView();
+            this.maKhachHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tenKhachHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ngaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chungMinhNhanDan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soDienThoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quocTich = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtmakhtp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBDatPhong = new System.Windows.Forms.GroupBox();
+            this.dtngaydattruoc = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtphongdt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBDatPhongTruoc = new System.Windows.Forms.CheckBox();
+            this.txtPhongtp = new System.Windows.Forms.TextBox();
+            this.dtTuNgaytp = new System.Windows.Forms.DateTimePicker();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tpTimKiem = new System.Windows.Forms.TabPage();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.dtgvTimKiem = new System.Windows.Forms.DataGridView();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.TKTheoDiaChi = new System.Windows.Forms.TextBox();
+            this.cbTheoDiaChi = new System.Windows.Forms.CheckBox();
+            this.TKTheoSoDT = new System.Windows.Forms.TextBox();
+            this.cbTheoDT = new System.Windows.Forms.CheckBox();
+            this.btTimKiem = new System.Windows.Forms.Button();
+            this.TKTheoCMND = new System.Windows.Forms.TextBox();
+            this.cbTheoCMND = new System.Windows.Forms.CheckBox();
+            this.TKTheoMa = new System.Windows.Forms.TextBox();
+            this.cbTheoMaKH = new System.Windows.Forms.CheckBox();
+            this.TKDenNgay = new System.Windows.Forms.DateTimePicker();
+            this.label13 = new System.Windows.Forms.Label();
+            this.TKTuNgay = new System.Windows.Forms.DateTimePicker();
+            this.TKTheoTen = new System.Windows.Forms.TextBox();
+            this.TKTheoPhong = new System.Windows.Forms.ComboBox();
+            this.cbThoiGian = new System.Windows.Forms.CheckBox();
+            this.cbTheoTen = new System.Windows.Forms.CheckBox();
+            this.cbTheoPhong = new System.Windows.Forms.CheckBox();
+            this.tpTraPhong = new System.Windows.Forms.TabPage();
+            this.groupBox9 = new System.Windows.Forms.GroupBox();
+            this.dgtraphong = new System.Windows.Forms.DataGridView();
+            this.groupBox8 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtmathuephong = new System.Windows.Forms.TextBox();
+            this.dtngaysinh = new System.Windows.Forms.DateTimePicker();
+            this.txttongtien = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtdongiatp = new System.Windows.Forms.TextBox();
+            this.dttpngaybatdauthue = new System.Windows.Forms.DateTimePicker();
+            this.label17 = new System.Windows.Forms.Label();
+            this.dttpngayketthucthue = new System.Windows.Forms.DateTimePicker();
+            this.label24 = new System.Windows.Forms.Label();
+            this.txtTPPhong = new System.Windows.Forms.TextBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.txtTPTenKH = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtTPSoCMND = new System.Windows.Forms.TextBox();
+            this.txtTPSoDT = new System.Windows.Forms.TextBox();
+            this.txtTPDiaChi = new System.Windows.Forms.TextBox();
+            this.TPTinhTien = new System.Windows.Forms.Button();
+            this.Tabphong.SuspendLayout();
+            this.tpDanhSanh.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDanhSachphong)).BeginInit();
+            this.tpThuePhong.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgkhachhang)).BeginInit();
+            this.groupBDatPhong.SuspendLayout();
+            this.tpTimKiem.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvTimKiem)).BeginInit();
+            this.groupBox6.SuspendLayout();
+            this.tpTraPhong.SuspendLayout();
+            this.groupBox9.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgtraphong)).BeginInit();
+            this.groupBox8.SuspendLayout();
+            this.SuspendLayout();
+
+
+
             // 
-            // dtgvDanhSachphong
+            // Tabphong
             // 
-            this.dtgvDanhSachphong.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dtgvDanhSachphong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgvDanhSachphong.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.maPhong,
-            this.loaiPhong,
-            this.tinhTrang,
-            this.donGia});
-            this.dtgvDanhSachphong.Location = new System.Drawing.Point(3, 52);
-            this.dtgvDanhSachphong.MultiSelect = false;
-            this.dtgvDanhSachphong.Name = "dtgvDanhSachphong";
-            this.dtgvDanhSachphong.ReadOnly = true;
-            this.dtgvDanhSachphong.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dtgvDanhSachphong.Size = new System.Drawing.Size(974, 331);
-            this.dtgvDanhSachphong.TabIndex = 1;
+            this.Tabphong.Controls.Add(this.tpDanhSanh);
+            this.Tabphong.Controls.Add(this.tpThuePhong);
+            this.Tabphong.Controls.Add(this.tpTimKiem);
+            this.Tabphong.Controls.Add(this.tpTraPhong);
+            this.Tabphong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Tabphong.Location = new System.Drawing.Point(12, 12);
+            this.Tabphong.Name = "Tabphong";
+            this.Tabphong.SelectedIndex = 0;
+            this.Tabphong.Size = new System.Drawing.Size(1010, 597);
+            this.Tabphong.TabIndex = 0;
+            this.Tabphong.Click += new System.EventHandler(this.Tabphong_Click);
             // 
-            // maPhong
+            // tpDanhSanh
             // 
-            this.maPhong.DataPropertyName = "maPhong";
-            this.maPhong.HeaderText = "Phòng";
-            this.maPhong.Name = "maPhong";
-            this.maPhong.ReadOnly = true;
-            // 
-            // loaiPhong
-            // 
-            this.loaiPhong.DataPropertyName = "loaiPhong";
-            this.loaiPhong.HeaderText = "Loại Phòng";
-            this.loaiPhong.Name = "loaiPhong";
-            this.loaiPhong.ReadOnly = true;
-            // 
-            // tinhTrang
-            // 
-            this.tinhTrang.DataPropertyName = "tinhTrang";
-            this.tinhTrang.HeaderText = "Tình trạng";
-            this.tinhTrang.Name = "tinhTrang";
-            this.tinhTrang.ReadOnly = true;
-            // 
-            // donGia
-            // 
-            this.donGia.DataPropertyName = "donGia";
-            this.donGia.HeaderText = "Đơn Giá";
-            this.donGia.Name = "donGia";
-            this.donGia.ReadOnly = true;
-            // 
-            // btndangxuat
-            // 
-            this.btndangxuat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.btndangxuat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btndangxuat.Location = new System.Drawing.Point(853, 20);
-            this.btndangxuat.Name = "btndangxuat";
-            this.btndangxuat.Size = new System.Drawing.Size(109, 26);
-            this.btndangxuat.TabIndex = 2;
-            this.btndangxuat.Text = "Đăng xuất";
-            this.btndangxuat.UseVisualStyleBackColor = false;
-            this.btndangxuat.Click += new System.EventHandler(this.btndangxuat_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.ForeColor = System.Drawing.Color.Fuchsia;
-            this.label2.Location = new System.Drawing.Point(267, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(426, 25);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "DANH SÁCH PHÒNG TRONG KHÁCH SẠN";
-            // 
-            // tpThuePhong
-            // 
+            this.tpDanhSanh.AccessibleName = "";
+            this.tpDanhSanh.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.tpDanhSanh.Controls.Add(this.dtgvDanhSachphong);
+            this.tpDanhSanh.Controls.Add(this.btndangxuat);
+            this.tpDanhSanh.Controls.Add(this.label2);
+            this.tpDanhSanh.Location = new System.Drawing.Point(4, 25);
+            this.tpDanhSanh.Name = "tpDanhSanh";
+            this.tpDanhSanh.Padding = new System.Windows.Forms.Padding(3);
+            this.tpDanhSanh.Size = new System.Drawing.Size(1002, 568);
+            this.tpDanhSanh.TabIndex = 0;
+            this.tpDanhSanh.Text = "Danh Sách";
+            this.tpDanhSanh.UseVisualStyleBackColor = true;
+
             this.tpThuePhong.Controls.Add(this.bHuyBo);
             this.tpThuePhong.Controls.Add(this.btThuePhongOK);
             this.tpThuePhong.Controls.Add(this.groupBox5);
