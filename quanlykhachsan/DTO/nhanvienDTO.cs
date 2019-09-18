adc507b7d67113b0370ea393e1212ac6e6f96f7b
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-18 16:40:29 +0700

    Update nhanvienDTO.cs

--- quanlykhachsan/DTO/nhanvienDTO.cs
+++ quanlykhachsan/DTO/nhanvienDTO.cs
@@ -8,5 +8,24 @@ namespace DTO
 {
     class nhanvienDTO
     {
+        private int _maNhanVien;
+        private string _hoTen;
+        private bool _gioiTinh;
+        private DateTime _ngaySinh;
+        private string _soChungMinh;
+        private string _diaChi;
+        private string _soDienThoai;
+        private DateTime _ngayVaoLam;
+
+        public int maNhanVien
+        {
+            get { return _maNhanVien; }
+            set { _maNhanVien = value; }
+        }
+        public string hoTen
+        {
+            get { return _hoTen; }
+            set { _hoTen = value; }
+        }
     }
 }
