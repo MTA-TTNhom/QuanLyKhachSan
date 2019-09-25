59ce7913f15f0863b7ef2faa36bb41aa4780dd5e
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-25 14:13:35 +0700

    update frmthongtin

--- quanlykhachsan/quanlykhachsan/frmthongtin.Designer.cs
+++ quanlykhachsan/quanlykhachsan/frmthongtin.Designer.cs
@@ -176,6 +176,36 @@
             this.label4.Size = new System.Drawing.Size(86, 19);
             this.label4.TabIndex = 4;
             this.label4.Text = "Số CMNN :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(441, 387);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 19);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Quốc tịch :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.Location = new System.Drawing.Point(441, 350);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 19);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Số điện thoại :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.Location = new System.Drawing.Point(441, 308);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 19);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Địa chỉ :";
         }
 
         #endregion
