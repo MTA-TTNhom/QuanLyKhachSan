fd8925b162b14371e66a06b1c531b4c718b9e5dd
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-10-03 21:24:36 +0700

    Update thuephongDAL.cs

--- quanlykhachsan/DAL/thuephongDAL.cs
+++ quanlykhachsan/DAL/thuephongDAL.cs
@@ -8,5 +8,26 @@ namespace DAL
 {
     class thuephongDAL
     {
+        public bool them_thuephong(int maKhachHang, DateTime ngayDen, int maPhong)
+        {
+            SqlConnection cnn = ketnoi.Get();
+            SqlCommand cmd = new SqlCommand("them_thuephong", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("maKhachHang", maKhachHang);
+            cmd.Parameters.AddWithValue("ngayDen", ngayDen);
+
+            cmd.Parameters.AddWithValue("maPhong", maPhong);
+
+            int i = cmd.ExecuteNonQuery();
+            cnn.Close();
+            if (i != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
